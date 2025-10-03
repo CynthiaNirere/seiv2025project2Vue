0c109e2245113bdc09ddcac736c776df3bf14ebf
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2025-10-03 10:55:01 -0500

    fixed naming

--- src/router.js
+++ src/router.js
@@ -6,16 +6,16 @@ const router = createRouter({
     { path: '/', name: 'Home', redirect: { name: 'CourseList' } },
 
     // LIST PAGE
-    { path: '/courses', name: 'CourseList', component: () => import('./views/CourseList.vue') },
+    { path: '/courses', name: 'CourseList', component: () => import('./views/courseList.vue') },
 
     // ADD
-    { path: '/courses/add', name: 'AddCourse', component: () => import('./views/AddCourse.vue') },
+    { path: '/courses/add', name: 'AddCourse', component: () => import('./views/addCourse.vue') },
 
     // DETAIL (optional if you need it)
-    { path: '/courses/:courseNumber', name: 'ViewCourse', component: () => import('./views/ViewCourse.vue'), props: true },
+    { path: '/courses/:courseNumber', name: 'ViewCourse', component: () => import('./views/viewCourse.vue'), props: true },
 
     // EDIT
-    { path: '/courses/:courseNumber/edit', name: 'EditCourse', component: () => import('./views/EditCourse.vue'), props: true },
+    { path: '/courses/:courseNumber/edit', name: 'EditCourse', component: () => import('./views/editCourse.vue'), props: true },
 
     // 404 fallback (optional)
     { path: '/:pathMatch(.*)*', redirect: { name: 'CourseList' } },
